5b96e9b70ffc785880522282e771fdf353b5f8e7
qzwlecr <qzwlecr@gmail.com>
2017-06-15 02:25:54 +0800

    complete reduce and mission 1 of Lab 1

--- src/mapreduce/common_reduce.go
+++ src/mapreduce/common_reduce.go
@@ -1,5 +1,14 @@
 package mapreduce
 
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"os"
+	"sort"
+)
+
 // doReduce manages one reduce task: it reads the intermediate
 // key/value pairs (produced by the map phase) for this task, sorts the
 // intermediate key/value pairs by key, calls the user-defined reduce function
@@ -11,36 +20,46 @@ func doReduce(
 	nMap int, // the number of map tasks that were run ("M" in the paper)
 	reduceF func(key string, values []string) string,
 ) {
-	//
-	// You will need to write this function.
-	//
-	// You'll need to read one intermediate file from each map task;
-	// reduceName(jobName, m, reduceTaskNumber) yields the file
-	// name from map task m.
-	//
-	// Your doMap() encoded the key/value pairs in the intermediate
-	// files, so you will need to decode them. If you used JSON, you can
-	// read and decode by creating a decoder and repeatedly calling
-	// .Decode(&kv) on it until it returns an error.
-	//
-	// You may find the first example in the golang sort package
-	// documentation useful.
-	//
-	// reduceF() is the application's reduce function. You should
-	// call it once per distinct key, with a slice of all the values
-	// for that key. reduceF() returns the reduced value for that key.
-	//
-	// You should write the reduce output as JSON encoded KeyValue
-	// objects to the file named outFile. We require you to use JSON
-	// because that is what the merger than combines the output
-	// from all the reduce tasks expects. There is nothing special about
-	// JSON -- it is just the marshalling format we chose to use. Your
-	// output code will look something like this:
-	//
-	// enc := json.NewEncoder(file)
-	// for key := ... {
-	// 	enc.Encode(KeyValue{key, reduceF(...)})
-	// }
-	// file.Close()
-	//
+	midContentBuf := bytes.NewBuffer(nil)
+	for maps := 0; maps < nMap; maps++ {
+		f, err := os.Open(reduceName(jobName, maps, reduceTaskNumber))
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			return
+		}
+		io.Copy(midContentBuf, f)
+		f.Close()
+	}
+	midContent := midContentBuf.Bytes()
+	reader := bytes.NewReader(midContent)
+	decoder := json.NewDecoder(reader)
+	var kv KeyValue
+	keyValues := make([]KeyValue, 0, 0)
+	for {
+		err := decoder.Decode(&kv)
+		if err == io.EOF {
+			break
+		}
+		keyValues = append(keyValues, kv)
+	}
+	keyValueMap := make(map[string][]string)
+	for _, keyValueSingle := range keyValues {
+		keyValueMap[keyValueSingle.Key] = append(keyValueMap[keyValueSingle.Key], keyValueSingle.Value)
+	}
+	keys := []string{}
+	for keyValueSingle := range keyValueMap {
+		keys = append(keys, keyValueSingle)
+	}
+	sort.Strings(keys)
+	answerFileName := mergeName(jobName, reduceTaskNumber)
+	answerFile, err := os.OpenFile(answerFileName, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0644)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		return
+	}
+	encoder := json.NewEncoder(answerFile)
+	for _, key := range keys {
+		encoder.Encode(KeyValue{key, reduceF(key, keyValueMap[key])})
+	}
+	answerFile.Close()
 }
